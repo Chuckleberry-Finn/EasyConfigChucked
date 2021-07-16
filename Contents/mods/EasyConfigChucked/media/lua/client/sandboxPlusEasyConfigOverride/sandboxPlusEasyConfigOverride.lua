5a2cb7ce67cb2474693275c51fc7e2a6d1e0d31f
ChuckTheJittai <ChuckTheSheep@users.noreply.github.com>
2021-07-15 21:02:40 -0400

    Update sandboxPlusEasyConfigOverride.lua

--- Contents/mods/EasyConfigChucked/media/lua/client/sandboxPlusEasyConfigOverride/sandboxPlusEasyConfigOverride.lua
+++ Contents/mods/EasyConfigChucked/media/lua/client/sandboxPlusEasyConfigOverride/sandboxPlusEasyConfigOverride.lua
@@ -1,9 +1,12 @@
 ---Override sandbox+ easyconfig entirely
+EasyConfig = EasyConfig or nil
 function sandboxPlusEasyConfigOverride()
-	for k,v in pairs(EasyConfig.mods) do
-		EasyConfig_Chucked.mods[k] = v
+	if EasyConfig then
+		for k,v in pairs(EasyConfig.mods) do
+			EasyConfig_Chucked.mods[k] = v
+		end
+		EasyConfig.mods = {}
 	end
-	EasyConfig.mods = {}
 end
 
 Events.OnGameBoot.Add(sandboxPlusEasyConfigOverride)
